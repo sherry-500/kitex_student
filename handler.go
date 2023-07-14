8865646a808436ea7a07bfecce6e9ebdcf872ec9
sherry-500 <1845351383@qq.com>
2023-07-14 22:24:38 +0800

    引入了gorm，实现了registry”

--- handler.go
+++ handler.go
@@ -2,27 +2,86 @@ package main
 
 import (
 	"context"
+	"errors"
+	"log"
+	//"time"
+	"strings"
+
 	demo "github.com/sherry-500/kitex_student/kitex_gen/demo"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 // StudentServiceImpl implements the last service interface defined in the IDL.
-type StudentServiceImpl struct{}
+type StudentServiceImpl struct{
+	db *gorm.DB
+}
+
+//Student entity
+type Student struct {
+	Id int32
+	Name string
+	Email string
+	CollegeName string
+	CollegeAddress string
+	//CreatedAt time.Time `gorm:"default:CURRENT_TMESTAMP`
+}
 
 // Register implements the StudentServiceImpl interface.
 func (s *StudentServiceImpl) Register(ctx context.Context, student *demo.Student) (resp *demo.RegisterResp, err error) {
 	// TODO: Your code here...
+	result := s.db.Table("students").Create(student2Model(student))
+	if result.Error != nil {
+		log.Fatal(result.Error)
+	}
 	return
 }
 
 // Query implements the StudentServiceImpl interface.
 func (s *StudentServiceImpl) Query(ctx context.Context, req *demo.QueryReq) (resp *demo.Student, err error) {
 	// TODO: Your code here...
-	resp = &demo.Student{
-		Id : 2,
-		Name : "sherry",
-		College: &demo.College{
-			Name: "nju",
-		},
+	var stuRes Student
+
+	result := s.db.Table("students").First(&stuRes, req.Id)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		resp = &demo.Student{}
+		panic("the result of query is null")
+	}else{
+		resp = &demo.Student{
+			Id : req.Id,
+			Name : stuRes.Name,
+			College: &demo.College{
+				Name: stuRes.CollegeName,
+				Address: stuRes.CollegeAddress,
+			},
+			Email: strings.Split(stuRes.Email, ","),
+		}
 	}
 	return
+}
+
+func (s *StudentServiceImpl) InitDB() {
+    db, err := gorm.Open(sqlite.Open("foo.db"), &gorm.Config{})
+    if err != nil {
+       panic(err)
+    }
+    // drop table
+    db.Migrator().DropTable(Student{})
+    // create table
+    err = db.Migrator().CreateTable(Student{})
+    if err != nil {
+       panic(err)
+    }
+    s.db = db
+}
+
+func student2Model(student *demo.Student) *Student{
+	stu := &Student{
+		Id : student.Id,
+		Name: student.Name,
+		CollegeName: student.College.Name,
+		CollegeAddress: student.College.Address,
+		Email : strings.Join(student.Email, ","),
+	}
+	return stu
 }
--- main.go
+++ main.go
@@ -6,15 +6,28 @@ import (
 	"log"
 	"net"
 	//"sever"
+	etcd "github.com/kitex-contrib/registry-etcd"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
 )
+
 
 func main() {
 	//svr := demo.NewServer(new(StudentServiceImpl))
+	r, err := etcd.NewEtcdRegistry([]string{"127.0.0.1:2379"})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	 addr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:8889")
-     svr := demo.NewServer(new(StudentServiceImpl), server.WithServiceAddr(addr))
+	addr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:8889")
 
-	err := svr.Run()
+	studentService := &StudentServiceImpl{}
+	studentService.InitDB()
+
+	svr := demo.NewServer(studentService, server.WithRegistry(r), server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: "studentservice"}), server.WithServiceAddr(addr))
+
+    //  svr := demo.NewServer(new(StudentServiceImpl), server.WithServiceAddr(addr))
+
+	err = svr.Run()
 
 	if err != nil {
 		log.Println(err.Error())
